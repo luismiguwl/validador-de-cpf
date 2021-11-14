40fba5bb5410a0374b7b9456343e3fd14d5088e0
luismiguwl <luisbwrb@gmail.com>
2021-11-13 23:45:58 -0300

    melhorando testes

--- src/test/java/com/luis/validador/core/ValidadorTest.java
+++ src/test/java/com/luis/validador/core/ValidadorTest.java
@@ -1,61 +1,111 @@
 package com.luis.validador.core;
 
-import com.luis.validador.exception.CPFInvalidoException;
 import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.DisplayName;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.luis.validador.exception.CPFInvalidoException;
 
 class ValidadorTest {
 
     Validador validador;
 
     @Test
-    @DisplayName("Testa todos os casos em que há possibilidade de lançar exceção")
-    public void todosOsCasosDeExcecao() {
-        String cpfComNumerosIguais = "11111111111";
-        String cpfComLetra = "40028922A18";
-        String cpfComLengthDiferenteDeOnze = "123";
-        
-        assertThatThrownBy(() -> validador = new Validador(cpfComNumerosIguais)).isInstanceOf(CPFInvalidoException.class);
-        assertThatThrownBy(() -> validador = new Validador(cpfComLetra)).isInstanceOf(CPFInvalidoException.class);
-        assertThatThrownBy(() -> validador = new Validador(cpfComLengthDiferenteDeOnze)).isInstanceOf(CPFInvalidoException.class);
+    @DisplayName("Deve retornar true se todos os números do CPF forem iguais")
+    public void cpfComNumerosIguaisTest() {
+        String cpf = "11111111111";
+        assertThatThrownBy(() -> validador = new Validador(cpf)).isInstanceOf(CPFInvalidoException.class);
     }
 
     @Test
-    @DisplayName("Testa todos os casos em que o CPF é válido ou inválido")
-    public void todosOsCasosEmQueOCPFEhValidoOuInvalido() {
-        String cpfValido = "529.982.247-25";
-        String cpfInvalido = "123.424.651-40";
-        
-        assertThat(new Validador(cpfValido).validar()).isTrue();
-        assertThat(new Validador(cpfInvalido).validar()).isFalse();
+    @DisplayName("Deve retornar exceção se CPF conter letra")
+    public void cpfPossuiLetraTest() {
+        String cpf = "12EA3567842";
+        assertThatThrownBy(() -> validador = new Validador(cpf)).isInstanceOf(CPFInvalidoException.class);
     }
 
     @Test
-    @DisplayName("Testa todos os casos em que há possibilidade de lançar exceção")
-    public void todosOsCasosEmQueOsDigitosSaoValidosOuInvalidos() {
-        String cpfComPrimeiroDigitoVerificadorValido = "529.982.247-25";
-        String cpfComPrimeiroDigitoVerificadorInvalido = "236.865.184-18";
-        String cpfComSegundoDigitoVerificadorValido = "529.982.247-25";
-        String cpfComSegundoDigitoVerificadorInvalido = "236.865.184-18";
-        
-        assertThat(new Validador(cpfComPrimeiroDigitoVerificadorValido).validarPrimeiroDigito()).isTrue();
-        assertThat(new Validador(cpfComPrimeiroDigitoVerificadorInvalido).validarPrimeiroDigito()).isFalse();
-        assertThat(new Validador(cpfComSegundoDigitoVerificadorValido).validarSegundoDigito()).isTrue();
-        assertThat(new Validador(cpfComSegundoDigitoVerificadorInvalido).validarSegundoDigito()).isFalse();
+    @DisplayName("Deve retornar exceção se CPF possuir length diferente de 11")
+    public void cpfComLengthDiferenteDeOnzeTest() {
+        String cpf = "112342";
+        assertThatThrownBy(() -> validador = new Validador(cpf)).isInstanceOf(CPFInvalidoException.class);
     }
 
     @Test
-    public void todosOsCasosEmQueHaMultiplicacaoDosNumerosDoCPF() {
+    @DisplayName("Deve retornar true se CPF for válido")
+    public void cpfValidoTest() {
+        String cpf = "529.982.247-25";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validar()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar false se CPF for inválido")
+    public void cpfInvalidoTest() {
+        String cpf = "123.424.651-40";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validar()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Deve retornar true se primeiro dígito for válido")
+    public void validarPrimeiroDigitoValidoTest() {
+        String cpf = "529.982.247-25";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validarPrimeiroDigito()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar false se primeiro dígito for inválido")
+    public void validarPrimeiroDigitoInvalidoTest() {
+        String cpf = "236.865.184-18";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validarPrimeiroDigito()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Deve retornar true se segundo dígito for válido")
+    public void validarSegundoDigitoValidoTest() {
+        String cpf = "529.982.247-25";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validarSegundoDigito()).isTrue();
+        assertThat(new Validador(cpf).validar()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar false se segundo dígito for inválido")
+    public void validarSegundoDigitoInvalidoTest() {
+        String cpf = "236.865.184-18";
+        validador = new Validador(cpf);
+
+        assertThat(validador.validarSegundoDigito()).isFalse();
+        assertThat(new Validador(cpf).validar()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Deve retornar o resultado da multiplicação dos números do CPF")
+    public void multiplicarNumeroDoCpfTest() {
         String cpf = "236.865.184-58";
         validador = new Validador(cpf);
-        
+
         int resultado = validador.obterSomaDaMultiplicacaoDosNumerosDoCpf(10, 2);
         assertThat(resultado).isEqualTo(248);
-        
-        resultado = validador.obterSomaDaMultiplicacaoDosNumerosDoCpf(11, 2);
+    }
+
+    @Test
+    @DisplayName("Deve retornar o resultado da multiplicação dos números do CPF #2")
+    public void multiplicarNumeroDoCpf2est() {
+        String cpf = "236.865.184-58";
+        validador = new Validador(cpf);
+
+        int resultado = validador.obterSomaDaMultiplicacaoDosNumerosDoCpf(11, 2);
         assertThat(resultado).isEqualTo(301);
     }
-    
+
 }
